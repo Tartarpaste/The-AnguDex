f37e257d0560e5a94e4063f2c9046faffc460168
Pavel Shwan Ibrahim <pavel.shwan.ibrahim@no.experis.com>
2023-01-31 13:38:29 +0100

    Added pokemon api url for 100 pokemons for each page in the Catalogue

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,4 +2,7 @@
 export const environment = {
     production: false,
     apiUsers: "https://glaze-thankful-wombat.glitch.me/trainers",
+    apiPokemon: "https://pokeapi.co/api/v2/pokemon?limit=100&offset=0"
 };
+
+//? The pokeAPI is limited to 100 (This is possibly for each page in the catalogue)
