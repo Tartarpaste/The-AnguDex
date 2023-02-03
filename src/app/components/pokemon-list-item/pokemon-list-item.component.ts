4ed4a0a2eb123a1ae7423935a97412b7a391b7a6
Tartarpaste <tordvg@hotmail.com>
2023-02-03 10:11:21 +0100

    merge cleanup

--- src/app/components/pokemon-list-item/pokemon-list-item.component.ts
+++ src/app/components/pokemon-list-item/pokemon-list-item.component.ts
@@ -1,5 +1,9 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, Input, OnInit } from '@angular/core';
 import { Pokemon } from 'src/app/models/pokemon.model';
+import { Trainer } from 'src/app/models/trainer.model';
+import { FavoriteService } from 'src/app/services/favorite.service';
+import { TrainerService } from 'src/app/services/trainer.service';
 
 @Component({
   selector: 'app-pokemon-list-item',
@@ -8,12 +12,31 @@ import { Pokemon } from 'src/app/models/pokemon.model';
 })
 export class PokemonListItemComponent implements OnInit {
   @Input() pokemon?: Pokemon;
+  @Input() pokemonID?: number = 0;
 
-  constructor() {}
+  public isCaught: boolean = false;
 
-  ngOnInit(): void {}
+  get loading(): boolean {
+    return this.favoriteService.loading;
+  }
+
+  constructor(
+    private trainerService: TrainerService,
+    private readonly favoriteService: FavoriteService
+  ) {}
+
+  ngOnInit(): void {
+    this.isCaught = this.trainerService.inFavorites(this.pokemonID!);
+  }
 
   onPokemonCatch(): void {
-    alert('caught ' + this.pokemon?.name + '!');
+    this.favoriteService.addToFavorites(this.pokemonID!).subscribe({
+      next: (response: Trainer) => {
+        console.log('NEXT', response);
+      },
+      error: (error: HttpErrorResponse) => {
+        console.log('ERROR', error.message);
+      },
+    });
   }
 }
