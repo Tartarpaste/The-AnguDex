d4248839f93b87df464ca4de1f03fda60f54feac
Pavel Shwan Ibrahim <pavel.shwan.ibrahim@no.experis.com>
2023-02-03 13:25:21 +0100

    Fixed envrionment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
     production: true,
     apiUsers: "https://glaze-thankful-wombat.glitch.me/trainers",
     apiPokemon: "https://pokeapi.co/api/v2/pokemon?limit=100&offset=0",
-    apiKey: "pyddeburger",
-    pokemonSprites: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"
+    apiPokemonAttributes: "https://pokeapi.co/api/v2/pokemon/",
+    apiKey: "pyddeburger"
 };
 
 //? The pokeAPI is limited to 100 (This is possibly for each page in the catalogue)
